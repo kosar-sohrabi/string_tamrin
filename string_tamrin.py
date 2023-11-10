0222f3e7995b04b82b13f500ab4330593ab68bc8
kosar sohrabi <145106308+kosar-sohrabi@users.noreply.github.com>
2023-11-10 22:28:07 +0330

    Add files via upload

---
+++ string_tamrin.py
@@ -0,0 +1,29 @@
+# data = 'From kosar.sohrabi@uct.ac.za Sat Jan  5 16:14:16 2023'
+# search1 = data.find('@')
+# print(search1)
+
+# search2 = data.find(' ',search1)
+# print(search2)
+
+# search3 = data[4+1 : search2]
+# print(search3)
+
+# ----------------------------------------------------------------------------------------
+
+# data2 = 'From kosar.sohrabi@uct.ac.za Sat Jan  5 16:14:16 2023'
+# print(len(data2))
+
+# print('.''\n''.')
+
+# split_data = data2.split()
+# print(split_data[7])
+
+# split_data = data2.split()
+# print(data2[7-2:])
+
+# split_data = data2.split()
+# print(data2['.':])
+
+# split_data = data2.split('.')
+# print(split_data[:])
+
