5159852a2d8cee953f8d9faaf98ba063af19d947
kosar sohrabi <145106308+kosar-sohrabi@users.noreply.github.com>
2023-11-26 21:45:48 +0330

    Update test_tamrin.py

--- test_tamrin.py
+++ test_tamrin.py
@@ -6,17 +6,19 @@ def salary(time , money):
 
 
 def over_pay(time , money):
-    over_time = time - 160
-    return over_time * (money * 1.5)
+    over_time = (time - 160)
+    money_test = (money * 1.5)
+    return over_time * money_test
 
 
 def regular(time , money):
+    t = 160
+    b = salary(t , money)
+    print("Your salary payment is :" ,b)
     if time > 160 :
         a = over_pay(time , money)
-        b = salary(time , money)
-        print("Your regular payment is :" , a+b)
+        print("your overpayment is :" , a)
     else :
         c = salary(time , money)
         print("Your regular payment is :" , c)
-
 regular(hour , rate)
